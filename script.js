801409c26d18613d054dbd190f2a418a7d41f84c
Jibonm37 <jibonrahman399@gmail.com>
2021-09-02 13:58:04 +0600

    error done

--- script.js
+++ script.js
@@ -1,39 +1,70 @@
+document.getElementById('error-message').style.display='none'
 const loadbooks = () => {
     const searchText = document.getElementById('search-text');
     const searchValue = searchText.value;
+ 
+    
     // console.log(searchValue)
     const url = `http://openlibrary.org/search.json?q=${searchValue}`
     fetch(url)
   .then(response => response.json())
   .then(data => displayBooks(data.docs))
+  document.getElementById('error-message').style.display='none';
   searchText.value = '';
+  
+   // Calling spinner
+  spinner('block');
 }
-// load books function --------------------
+
+// spinner area ---------------------
+const spinner = displaySpinner => {
+  document.getElementById('spinner').style.display= displaySpinner;
+}
 loadbooks()
+// display books function --------------------
+
 const displayBooks = books => {
     const allResult = document.getElementById('all-result');
+    
+
     allResult.textContent= '';
-    console.log(books.title)
-    books.forEach(book => {
-        const item = document.createElement('div');
-        item.classList.add('col')
-        item.innerHTML = `
-        <div class="card h-100">
-                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                
-                <h1 class="card-title">Book Name:- ${book.title}</h1>
-                  <h3 class="card-title"> Publish Year:- ${book.publish_year}</h3>
-                  <h5 class="card-title"> Author Name :-  ${book.author_name}</h5>
+    if(books.length === 0){
+      document.getElementById('error-message').style.display='block'
+    }
+    else{
+
+
+      console.log(books.title)
+      books?.forEach(book => {
+          const item = document.createElement('div');
+          item.classList.add('col')
+          item.innerHTML = `
+          <div class="card h-100">
+                  <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
                   
+                  <h1 class="card-title">Book Name:- ${book.title}</h1>
+                    <h3 class="card-title"> Publish Year:- ${book.publish_year}</h3>
+                    <h5 class="card-title"> Author Name :-  ${book.author_name}</h5>
+                    
+                  </div>
                 </div>
-              </div>
-        
-        `
-        
-        console.log(book)
-        allResult.appendChild(item);
-    })
-    // console.log(allResult)
+          
+          `
+          
+          console.log(book)
+          allResult.appendChild(item);
+      })
+
+
+
+
+    }
+   
+    
+    // Calling spinner
+    spinner('none')
+    
+    
     
 }
